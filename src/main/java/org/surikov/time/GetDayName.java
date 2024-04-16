f5848aa91a1bcf4687b93faaa3e9bb6f30f5e1f4
Evgenii Surikov <surikov96@mail.ru>
2024-04-16 21:08:20 +0700

    Удалил случайно написанную аннотацию раньше срока

--- src/main/java/org/surikov/time/GetDayName.java
+++ src/main/java/org/surikov/time/GetDayName.java
@@ -1,14 +1,12 @@
 package org.surikov.time;
 
-import org.springframework.stereotype.Component;
-
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
-@Component
+
 public class GetDayName {
 
     public String getTime(LocalDate date, Locale locale) {
